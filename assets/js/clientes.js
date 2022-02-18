4dc8f47c025bbb4298839a18a7fa991a83b926b6
conrado_carrillo <anton_con@hotmail.com>
2022-02-18 11:05:35 -0600

    Modificaciones en el form de clientes

--- assets/js/clientes.js
+++ assets/js/clientes.js
@@ -119,51 +119,104 @@ function getAddressOnList() {
 }
 // Guarda la información de un cliente en Netsuite
 function saveCustomer(){
-    let tipoRegimen     = $("input[name=tipoRegimen]:checked").val();
-    let requiereFactura = $("input[name=requiereFactura]:checked").val();
-    let middleName      = tipoRegimen != 'domestico' ? $('input#giroNegocioFormCliente').val() : "";
-    let lastName        = $('input#apellidoPaternoFormCliente').val()+' '+$('input#apellidoMaternoFormCliente').val();
-    let rfc             = requiereFactura == 'si' ? $('input#rfcFormCliente').val() : "";
-    let email           = $("input#correoFormCliente").val();
-    let emailAlt        = $("input#correoAlternativoFormCliente").val();
-
-    let customer        = {
-        nombre : $('input#nombreFormCliente').val(),
-        middleName : middleName,
-        lastName : lastName.trim(),
-        rfc : rfc,
-        regimeType : tipoRegimen,
-        email : email,
-        emails : {
-            principal : email,
-            adicionales : emailAlt
-        },
-        planta : "",
-        telefono : $("input#telefonoPrincipalFormCliente").val(),
-        telefonoAlt : $("input#telefonoAlternoFormCliente").val(),
-        subsidiaria : "",
-        regimenId : "",
-        typeService : "",
-        address : listAddress
-        // address : [
-        //     {
-        //         principal : "",
-        //         domFacturacion : "",
-        //         city : "",
-        //         stateName : "",
-        //         ruta : "",
-        //         nameStreet : "",
-        //         numExterno : "",
-        //         numInterno: "",
-        //         colonia : "",
-        //         latitud : "",
-        //         longitud : "",
-        //         street_aux1 : "",
-        //         street_aux2 : "",
-        //         idRoute : "",
-        //         zip : ""
-        //     }
-        // ]
+    let canContinue = false;
+    if(
+        !$("#cadaFormCliente").val().trim()                         ||
+        !$("#frecuenciaFormCliente").val().trim()                   ||
+        !$("#entreFormCliente").val().trim()                        ||
+        !$("#lasFormCliente").val().trim()                          ||
+        $("input[name=tipoAccionFormCliente]:checked").val().trim() ||
+    ) {
+        canContinue = false;
+    } else {
+        canContinue = true;
     }
-    console.log(customer);
+
+    if ( canContinue ) {
+        let tipoRegimen     = $("input[name=tipoRegimen]:checked").val();
+        let requiereFactura = $("input[name=requiereFactura]:checked").val();
+        let middleName      = tipoRegimen != 'domestico' ? $('input#giroNegocioFormCliente').val() : "";
+        let lastName        = $('input#apellidoPaternoFormCliente').val()+' '+$('input#apellidoMaternoFormCliente').val();
+        let rfc             = requiereFactura == 'si' ? $('input#rfcFormCliente').val() : "";
+        let email           = $("input#correoFormCliente").val();
+        let emailAlt        = $("input#correoAlternativoFormCliente").val();
+        let service         = $("input#tipoServicioFormCliente").val();
+        let typeService     = null;
+        let regimenId       = null;
+
+        if( tipoRegimen == 'industrial'){ regimenId = 1; }
+        else if( tipoRegimen == 'domestico'){ regimenId = 3; }
+        else if( tipoRegimen == 'industrial'){ regimenId = 5; }
+
+        if( service == 'cilindro' ){ typeService = 1; }
+        else if( service == 'estacionario' ){ typeService = 2; }
+        else if( service == 'ambos' ){ typeService = 4; }
+
+        let customer = {
+            nombre : $('input#nombreFormCliente').val(),
+            middleName : middleName,
+            lastName : lastName.trim(),
+            rfc : rfc,
+            regimeType : false,
+            // regimeType : tipoRegimen,
+            email : email,
+            emails : {
+                principal : email,
+                adicionales : emailAlt
+            },
+            planta : $('select#plantas').val(),
+            telefono : $("input#telefonoPrincipalFormCliente").val(),
+            telefonoAlt : $("input#telefonoAlternoFormCliente").val(),
+            subsidiaria : 16,// Seteado de forma estática
+            regimenId : regimenId,
+            typeService : typeService,
+            address : listAddress
+        }
+        console.log(customer);
+    } else {
+        alert("Favor de llenar todos los campos con *");
+    }
+}
+
+// Guarda la información de un cliente
+function saveCustomerPut() {
+    let customStartTime = customEndTime = new Date();
+    let yLas            = $("#lasFormCliente").val();
+    let entreLas        = $("#entreFormCliente").val();
+    let periodoContacto = $("select#frecuenciaFormCliente").val();
+    let horaInicio      = entreLas.split(':');
+    let horaFin         = yLas.split(':');
+    let horaInicioStr = horaFinStr = "";
+
+    if ( horaInicio.length ) {
+        customStartTime.setHours(horaInicio[0]);
+        customStartTime.setMinutes(horaInicio[1]);
+        horaInicioStr = moment(customStartTime).format('hh:mm a');
+        console.log('Hora inicio: '+ horaInicioStr);
+    }
+
+    if ( horaFin.length ) {
+        customEndTime.setHours(horaFin[0]);
+        customEndTime.setMinutes(horaFin[1]);
+        horaFinStr = moment(customEndTime).format('hh:mm a');
+        console.log('Hora fin: '+ horaFinStr);
+    }
+
+    let body = {
+        custentity_ptg_cada_ : $("input#cadaFormCliente").val(),
+        custentity_ptg_periododecontacto_ : periodoContacto,
+        custentity_ptg_entrelas_ : horaInicioStr,
+        custentity_ptg_ylas_ : horaFinStr,
+        custentity_ptg_lunes_ : $("#lunesFormCliente").is(':checked'),
+        custentity_ptg_martes_ :$("#martesFormCliente").is(':checked'),
+        custentity_ptg_miercoles_ : $("#miercolesFormCliente").is(':checked'),
+        custentity_ptg_jueves_ : $("#juevesFormCliente").is(':checked'),
+        custentity_ptg_viernes_ : $("#viernesFormCliente").is(':checked'),
+        custentity_ptg_sabado_ : $("#sabadoFormCliente").is(':checked'),
+        custentity_ptg_domingo_ : $("#domingoFormCliente").is(':checked'),
+        custentityptg_tipodecontacto_ : $("input[name=tipoAccionFormCliente]:checked").val(),
+        custentity_ptg_indicaciones_cliente : $("#indicacionesFormCliente").val()
+    }
+
+    console.log(body);
 }
